4182f03f96abe60c425e6f5f84290c8244f6af86
rakib alam <rakib.alam@dsinnovators.com>
2025-01-16 17:17:36 +0600

    anisul islam asp.net till part 6

--- Program.cs
+++ Program.cs
@@ -1,9 +1,53 @@
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// /// For swagger api testing
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+/// For swagger api testing
+if(app.Environment.IsDevelopment()){
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection( );
+
+var products = new List<Product>(){
+    new Product("Samsung20", 1240),
+    new Product("Nokia", 1000)
+};
+
+app.MapGet("/products", ()=>{
+    return Results.Ok(products);
+});
+
+app.MapGet("/hello", ()=>{
+    var response = new { message = "This is a json object", success = true};
+    return Results.Ok(response);
+});
+
+app.MapGet("/", ()=>{
+    return Results.Content("<h1> Hello World </h2>", "text/html");
+});
+
+app.MapPost("/", ()=>{
+    return Results.Ok();
+});
+
+app.MapPut("/", ()=>{
+    return Results.Created();
+});
+ 
+app.MapDelete("/", ()=>{
+    return Results.NoContent();
+});
 
 app.Run();
 
+public record Product(string Name, decimal Price);
  
